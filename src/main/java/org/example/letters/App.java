7eaa36f45441431221df0b1bb9b533ab9b5a5558
XrazYang <1161051479@qq.com>
2023-07-14 14:26:46 +0800

    表扬信.

--- src/main/java/org/example/letters/App.java
+++ src/main/java/org/example/letters/App.java
@@ -7,6 +7,6 @@ import java.io.IOException;
  */
 public class App {
     public static void main(String[] args) throws IOException {
-        ExcelReader.readExcel("D:\\dev\\Java\\ELearning\\src\\main\\resources\\表扬信\\151353199_2_表扬信汇总表_147_147.xlsx");
+        ExcelReader.readExcel(args[0]);
     }
 }
